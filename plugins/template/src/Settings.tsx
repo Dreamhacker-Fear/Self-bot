07e246ac25d15f0ccbb51c7d015aff439def232e
Rᴏᴏᴛᴋɪᴛ™ <lucaebraeuer@gmail.com>
2026-08-28 05:18:48 +0200

    Implement settings page with command toggles

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,8 +1,55 @@
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
+import { React } from "@vendetta/metro/common";
+import { General, Forms } from "@vendetta/ui/components";
+import { storage } from "@vendetta/plugin";
 
-export default () => (
-    <FormText>
-        Hello, world!
-    </FormText>
-)
+const { ScrollView } = General;
+const { FormSection, FormSwitchRow, FormDivider } = Forms;
+
+const COMMAND_LIST = [
+    "avatar",
+    "roll",
+    "8ball",
+    "coinflip",
+    "color",
+    "choose",
+    "myid",
+    "timestamp",
+    "remindme",
+    "petpet",
+    "triggered",
+    "deepfry",
+];
+
+function loadEnabled() {
+    storage.enabledCommands = storage.enabledCommands ?? Object.fromEntries(
+        COMMAND_LIST.map((name) => [name, true])
+    );
+    return storage.enabledCommands;
+}
+
+export default function Settings() {
+    const [enabled, setEnabled] = React.useState(loadEnabled());
+
+    const toggle = (name, value) => {
+        const merged = { ...enabled, [name]: value };
+        setEnabled(merged);
+        storage.enabledCommands = merged;
+    };
+
+    return (
+        <ScrollView>
+            <FormSection title="Commands">
+                {COMMAND_LIST.map((name, i) => (
+                    <React.Fragment key={name}>
+                        <FormSwitchRow
+                            label={`/${name}`}
+                            value={enabled[name]}
+                            onValueChange={(v) => toggle(name, v)}
+                        />
+                        {i < COMMAND_LIST.length - 1 && <FormDivider />}
+                    </React.Fragment>
+                ))}
+            </FormSection>
+        </ScrollView>
+    );
+}
